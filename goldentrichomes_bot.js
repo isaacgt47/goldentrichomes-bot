f6f55b2b83a6f48648fdd039e53aa7c7c8ce81a7
isaacgt47 <yjemli99@gmail.com>
2026-07-30 18:28:49 +0200

    Update goldentrichomes_bot.js

--- goldentrichomes_bot.js
+++ goldentrichomes_bot.js
@@ -9,8 +9,8 @@ const axios       = require('axios');
    ══════════════════════════════════════════ */
 const CONFIG = {
   BOT_TOKEN:    process.env.BOT_TOKEN   || '8689166931:AAFweXM9nYW9YoY6-W0INnNURCCXpJ7bMjU',
-  ADMIN_CHAT:   process.env.ADMIN_CHAT  || '-5108947245',   /* ton Chat ID perso */
-  GROUP_CHAT:   process.env.GROUP_CHAT  || '-5108947245',  /* ton groupe commandes */
+  ADMIN_CHAT:   process.env.ADMIN_CHAT  || '5108947245',   /* ton Chat ID perso */
+  GROUP_CHAT:   process.env.GROUP_CHAT  || '5108947245',  /* ton groupe commandes */
   WEBHOOK_URL:  process.env.WEBHOOK_URL || 'https://goldentrichomes-bot-production.up.railway.app',
   MINI_APP_URL: 'https://melodic-baklava-cd5a09.netlify.app/',
   PORT:         process.env.PORT        || 3000,

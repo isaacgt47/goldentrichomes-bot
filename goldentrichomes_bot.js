18b9f060f3a09c78d2ed8b58cb107207526c733e
isaacgt47 <yjemli99@gmail.com>
2026-07-31 22:45:48 +0200

    Update goldentrichomes_bot.js

--- goldentrichomes_bot.js
+++ goldentrichomes_bot.js
@@ -56,6 +56,13 @@ try {
 const bot = new TelegramBot(CONFIG.BOT_TOKEN, { polling: false });
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 app.get('/', (req, res) => res.json({ status: 'GoldenTrichomes Bot Online 🌿' }));
 
